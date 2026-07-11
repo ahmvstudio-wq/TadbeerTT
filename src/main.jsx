31732814377369aa9dd88e6950ce2d6067836ab1
Mohammed Rehan <rehan@tadbeertt.com>
2026-07-11 23:26:33 +0530

    Configure Sentry monitoring

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,19 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://de5a8c565eee16e3b534a9179782628a@o4511717942296576.ingest.us.sentry.io/4511717948456960",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
